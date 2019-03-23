2e11abb11912cb42a1bddfe22803c05d713cbec8
Erik Dao <erik.dq.cuong@gmail.com>
2019-03-24 01:15:51 +0800

    Move class properties to constructor. Use uuid to id action and role.

--- raise_player.py
+++ raise_player.py
@@ -3,30 +3,33 @@ from time import sleep
 from pprint import pprint
 
 class RaisedPlayer(BasePokerPlayer):
-  # name of our bot, I think hardcode cause looking at the round_state data there's no other way to identify whose action is who
-  name = "FT2" 
-  round = 0 # current_round
-  seat = 0
-  update_sb = True
-  small_blind = True
 
-  opp_action = True # odd-even hack to check if the game_update is opponent action
+  def __init__(self):
+      self.uuid = ""
+      self.round = 0 # current_round
+      self.small_blind = True
+      self.first_action = True # of a new round, occur in preflop, to determine who's small_blind
 
-  # opponents properties for classification (loose/tight, passive/aggressive)
-  # loose if (round - folded)/round (GP%) >= 28%
-  # passive if raise_count - call_count <= 0
-  # 4 stages: preflop, flop, turn, river
-  folded = 0 # games where opponent fold
-  raise_count = 0 # number of hands raised of opponent
-  call_count = 0  
+      # opponents properties for classification (loose/tight, passive/aggressive)
+      # loose if (round - folded)/round (GP%) >= 28%
+      # passive if raise_count - call_count <= 0
+      # 4 stages: preflop, flop, turn, river
+      self.opp_folded = 0 # games where opponent fold
+      self.opp_raise = 0 # number of hands raised of opponent
+      self.opp_call = 0  
 
   def declare_action(self, valid_actions, hole_card, round_state):
     
+    if self.first_action: # our bot has 1st action (b4 receiving 1st game update) -> bot is small_blind
+      self.small_blind = True
+      self.uuid = round_state['action_histories']['preflop'][0]['uuid']
+      self.first_action = False
+
     opp_class = self.update_opponent_classifification()
 
-    # print("\n")
-    # pprint(opp_class)
-    # print("\n")
+    #print("\nraise_count: " + str(self.opp_raise))
+    #print("call_count: " + str(self.opp_call))
+    #pprint(opp_class)
 
     for i in valid_actions:
         if i["action"] == "raise":
@@ -36,61 +39,45 @@ class RaisedPlayer(BasePokerPlayer):
     return action # action returned here is sent to the poker engine
 
   def receive_game_start_message(self, game_info):
-    if game_info['seats'][0]['name'] == self.name:
-      self.seat = 0
-    else:
-      self.seat = 1
+    self.first_action = True
 
   def receive_round_start_message(self, round_count, hole_card, seats):
     self.round = round_count
-    self.update_sb = True # Flag signalling new ground to update seat position
     
 
   def receive_street_start_message(self, street, round_state):
-    # small rant: at every round start should have info containing who's the small blind & big blind
-    # but nope round_state doesn't get pass so have to check here, questionable original code decision
-    if self.update_sb:
-      if round_state['small_blind_pos'] == self.seat:
-        self.small_blind = True
-        self.opp_action = False # odd-even switch, opponent big_blind so start 2nd each street
-      else:
-        self.small_blind = False
-        self.opp_action = True
-      update_sb = False
+    pass
 
   def receive_game_update_message(self, action, round_state):
-    if self.opp_action:
-      action = round_state['action_histories'][round_state['street']][-1]['action']
-      if action == "RAISE":
-        self.raise_count += 1
-      if action == "CALL":
-        self.call_count += 1
 
-    old_bool = self.opp_action
-    self.opp_action = not old_bool # switch since next action would be ours
+    if self.first_action: # got 1st game update before declare 1st action -> opponent small_blind
+      self.small_blind = False
+      self.uuid = round_state['action_histories']['preflop'][1]['uuid']
+      self.first_action = False
 
-    # logFile = open('logfile.txt', 'a')
-    # pprint(round_state, logFile)
+    if action['player_uuid'] != self.uuid:
+      opp_action = action['action']
+      if opp_action == 'raise':
+        self.opp_raise += 1
+      if opp_action == 'call':
+        self.opp_call += 1
+      if opp_action == 'fold':
+        self.opp_folded += 1      
     
   def receive_round_result_message(self, winners, hand_info, round_state):
-    logFile = open('logfile.txt', 'a')
-    pprint(round_state, logFile)
-    opponent_seat = 1 - self.seat
-    if round_state['seats'][opponent_seat]['state'] == "folded":
-      self.folded += 1
-
-    # print("\n Total opponent fold " + str(self.folded))
-    # print("Total opponent raise: " + str(self.raise_count))
-    # print("Total opponent call: " + str(self.call_count) + "\n")
+    # logFile = open('logfile.txt', 'a')
+    # pprint(round_state, logFile)
+    # print("opp fold: " + str(self.opp_folded))
+    pass
 
   def update_opponent_classifification(self):
     tightness = True
-    GP_percentage = (self.round - self.folded) / float(self.round)
+    GP_percentage = (self.round - self.opp_folded) / float(self.round)
     if GP_percentage < 0.28:
       tightness = False
     
     aggressiveness = True
-    if self.raise_count - self.call_count <= 0:
+    if self.opp_raise - self.opp_call <= 0:
       aggressiveness = False
 
     return {
@@ -102,15 +89,13 @@ class RaisedPlayer(BasePokerPlayer):
   def get_pot(self, round_state):
     return round_state['pot']['main']['amount']      
 
-  def get_seat_position(self, round_state):
-    if round_state['seats'][0]['name'] == self.name:
-      return 0
-    else:
-      return 1
-
   # Get our own stack. For opponent stack = 1000 - our stack - pot
   def get_stack(self, round_state):
-    return round_state['seats'][seat]['stack'] 
+    seats = round_state['seats']
+    if seats[0]['uuid'] == self.uuid:
+      return seats[0]['stack']
+    else:
+      return seats[1]['stack'] 
 
 def setup_ai():
   return RaisePlayer()
