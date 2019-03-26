db20fe1c02f40908dd4c3c66535b5ac79c5921d9
jlks96 <joshlee_1996@hotmail.com>
2019-03-26 15:50:52 +0800

    -fixed indentation

--- randomplayer.py
+++ randomplayer.py
@@ -34,7 +34,6 @@ class RandomPlayer(BasePokerPlayer):
         self.final_reward = 0
         self.beginning_stack = 0
         self.final_stack = 0
-        self.uuid = ""
         # ---------------INFO FOR EHS ---------------- #
         with open('file.txt') as f:
           raw_table = f.readlines()[0]
@@ -52,8 +51,8 @@ class RandomPlayer(BasePokerPlayer):
         # 4 stages: preflop, flop, turn, river
         self.opp_folded = 0 # games where opponent fold
         self.opp_raise = 0 # number of hands raised of opponent
-        self.opp_call = 0  
-          
+        self.opp_call = 0
+
     def declare_action(self, valid_actions, hole_card, round_state):
         #----------OPP CLASS----------#
         if self.first_action: # our bot has 1st action (b4 receiving 1st game update) -> bot is small_blind
@@ -62,7 +61,7 @@ class RandomPlayer(BasePokerPlayer):
             self.first_action = False
 
         opp_class = self.update_opponent_classifification()
-        
+
         #----------PREDICT ACTION---------#
         state = array([0, 0, 0, 0, 0])  # TODO: compute the state ie feature values (SOMEBODY DO THIS PLEASE)
         state = np.reshape(state, [1, self.state_size])
@@ -105,7 +104,7 @@ class RandomPlayer(BasePokerPlayer):
             if opp_action == 'call':
                 self.opp_call += 1
             if opp_action == 'fold':
-                self.opp_folded += 1      
+                self.opp_folded += 1
 
     def receive_round_result_message(self, winners, hand_info, round_state):
         for s in round_state['seats']:
@@ -166,19 +165,19 @@ class RandomPlayer(BasePokerPlayer):
 
     def save(self, name):
         self.model.save_weights(name)
-    
+
     # -------------------EHS------------------ #
     '''POLYFILL FOR ITERTOOLS.COMBINATIONS'''
     def combinations(self, iterable, r):
-      # combinations('ABCD', 2) --> AB AC AD BC BD CD
-      # combinations(range(4), 3) --> 012 013 023 123
-      pool = tuple(iterable)
-      n = len(pool)
-      if r > n:
+        # combinations('ABCD', 2) --> AB AC AD BC BD CD
+        # combinations(range(4), 3) --> 012 013 023 123
+        pool = tuple(iterable)
+        n = len(pool)
+        if r > n:
           return
-      indices = list(range(r))
-      yield tuple(pool[i] for i in indices)
-      while True:
+        indices = list(range(r))
+        yield tuple(pool[i] for i in indices)
+        while True:
           for i in reversed(range(r)):
               if indices[i] != i + n - r:
                   break
@@ -190,109 +189,109 @@ class RandomPlayer(BasePokerPlayer):
           yield tuple(pool[i] for i in indices)
 
     def EHS_0(self, hole_card):
-      card1 = hole_card[0]
-      card2 = hole_card[1]
-      suited = card1[0] == card2[0]
-      probability = self.hole_table.get((card1[1], card2[1], suited)) if self.hole_table.get((card1[1], card2[1], suited)) != None else self.hole_table.get((card2[1], card1[1], suited))
-      return probability/100
+        card1 = hole_card[0]
+        card2 = hole_card[1]
+        suited = card1[0] == card2[0]
+        probability = self.hole_table.get((card1[1], card2[1], suited)) if self.hole_table.get((card1[1], card2[1], suited)) != None else self.hole_table.get((card2[1], card1[1], suited))
+        return probability/100
 
     def EHS_3_4(self, hole_card, community_card):
-      p_win = 0
-      for iter in range(1000):
-        community_card_new, opp_hole_card_new = self.generate_cards(hole_card, community_card)
-        hole_card_new = [Card.from_str(card) for card in hole_card]
-        p_score = HandEvaluator.eval_hand(hole_card_new, community_card_new)
-        o_score = HandEvaluator.eval_hand(opp_hole_card_new, community_card_new)
-        p_win += int(p_score > o_score)
-      return p_win/1000
+        p_win = 0
+        for iter in range(1000):
+            community_card_new, opp_hole_card_new = self.generate_cards(hole_card, community_card)
+            hole_card_new = [Card.from_str(card) for card in hole_card]
+            p_score = HandEvaluator.eval_hand(hole_card_new, community_card_new)
+            o_score = HandEvaluator.eval_hand(opp_hole_card_new, community_card_new)
+            p_win += int(p_score > o_score)
+        return p_win/1000
 
     def EHS_5(self, hole_card, community_card):
-      opp_possible_hole_cards = self.get_all_possible_opp_hole(hole_card, community_card)
-      p_win = 0
-      hole_card_new = [Card.from_str(card) for card in hole_card]
-      community_card_new = [Card.from_str(card) for card in community_card]
-      for opp_hole_card in opp_possible_hole_cards:
-        p_score = HandEvaluator.eval_hand(hole_card_new, community_card_new)
-        o_score = HandEvaluator.eval_hand(opp_hole_card, community_card_new)
-        p_win += int(p_score > o_score)
-      return p_win/len(opp_possible_hole_cards) # SHOULD I DO LAPLACE TO PREVENT N/0 SITUATION?
+        opp_possible_hole_cards = self.get_all_possible_opp_hole(hole_card, community_card)
+        p_win = 0
+        hole_card_new = [Card.from_str(card) for card in hole_card]
+        community_card_new = [Card.from_str(card) for card in community_card]
+        for opp_hole_card in opp_possible_hole_cards:
+            p_score = HandEvaluator.eval_hand(hole_card_new, community_card_new)
+            o_score = HandEvaluator.eval_hand(opp_hole_card, community_card_new)
+            p_win += int(p_score > o_score)
+        return p_win/len(opp_possible_hole_cards) # SHOULD I DO LAPLACE TO PREVENT N/0 SITUATION?
 
     def EHS(self, hole_card, community_card):
-      if len(community_card) == 0:
-        return self.EHS_0(hole_card)
-      if len(community_card) == 3 or len(community_card) == 4:
-        return self.EHS_3_4(hole_card, community_card)
-      else:
-        return self.EHS_5(hole_card, community_card)
+        if len(community_card) == 0:
+            return self.EHS_0(hole_card)
+        if len(community_card) == 3 or len(community_card) == 4:
+            return self.EHS_3_4(hole_card, community_card)
+        else:
+            return self.EHS_5(hole_card, community_card)
 
     def generate_cards(self, hole_card, community_card):
-      community_card_new = [card for card in community_card]
-      original_deck_set = set(range(1,53))
-      hole_card_set = set([Card.from_str(card).to_id() for card in hole_card])
-      community_card_set = set([Card.from_str(card).to_id() for card in community_card])
-      revealed_set = hole_card_set.union(community_card_set)
-      cheat_deck = list(original_deck_set - revealed_set) # TO TEST!!!!
-      opp_hole_card = []
-      while(len(community_card_new) < 5):
-        idx = rand.choice(cheat_deck)
-        new_card = Card.from_id(idx).__str__()
-        community_card_new.append(new_card)
-        cheat_deck.remove(idx)
-      while(len(opp_hole_card) < 2):
-        idx = rand.choice(cheat_deck)
-        new_card = Card.from_id(idx).__str__()
-        opp_hole_card.append(new_card)
-        cheat_deck.remove(idx)
-      community_card_new = [Card.from_str(card) for card in community_card_new]
-      opp_hole_card = [Card.from_str(card) for card in opp_hole_card]
-      return community_card_new, opp_hole_card 
+        community_card_new = [card for card in community_card]
+        original_deck_set = set(range(1,53))
+        hole_card_set = set([Card.from_str(card).to_id() for card in hole_card])
+        community_card_set = set([Card.from_str(card).to_id() for card in community_card])
+        revealed_set = hole_card_set.union(community_card_set)
+        cheat_deck = list(original_deck_set - revealed_set) # TO TEST!!!!
+        opp_hole_card = []
+        while(len(community_card_new) < 5):
+            idx = rand.choice(cheat_deck)
+            new_card = Card.from_id(idx).__str__()
+            community_card_new.append(new_card)
+            cheat_deck.remove(idx)
+        while(len(opp_hole_card) < 2):
+            idx = rand.choice(cheat_deck)
+            new_card = Card.from_id(idx).__str__()
+            opp_hole_card.append(new_card)
+            cheat_deck.remove(idx)
+        community_card_new = [Card.from_str(card) for card in community_card_new]
+        opp_hole_card = [Card.from_str(card) for card in opp_hole_card]
+        return community_card_new, opp_hole_card
 
     def get_all_possible_opp_hole(self, hole_card, community_card):
-      original_deck_set = set(range(1,53))
-      hole_card_set = set([Card.from_str(card).to_id() for card in hole_card])
-      community_card_set = set([Card.from_str(card).to_id() for card in community_card])
-      revealed_set = hole_card_set.union(community_card_set)
-      cheat_deck = list(original_deck_set - revealed_set) # TO TEST!!!!
-      opp_possible_hole_card_id= self.combinations(cheat_deck, 2)
-      opp_possible_hole_cards = [[Card.from_id(id_pair[0]), Card.from_id(id_pair[1])] for id_pair in opp_possible_hole_card_id]
-      return opp_possible_hole_cards
-      
+        original_deck_set = set(range(1,53))
+        hole_card_set = set([Card.from_str(card).to_id() for card in hole_card])
+        community_card_set = set([Card.from_str(card).to_id() for card in community_card])
+        revealed_set = hole_card_set.union(community_card_set)
+        cheat_deck = list(original_deck_set - revealed_set) # TO TEST!!!!
+        opp_possible_hole_card_id= self.combinations(cheat_deck, 2)
+        opp_possible_hole_cards = [[Card.from_id(id_pair[0]), Card.from_id(id_pair[1])] for id_pair in opp_possible_hole_card_id]
+        return opp_possible_hole_cards
+
     # -------------------OPP CLASSIFICATION------------------ #
-  def update_opponent_classifification(self):
-    tightness = True
-    GP_percentage = (self.round - self.opp_folded) / float(self.round)
-    if GP_percentage < 0.28:
-      tightness = False
-    
-    aggressiveness = True
-    if self.opp_raise - self.opp_call <= 0:
-      aggressiveness = False
+    def update_opponent_classifification(self):
+        tightness = True
+        GP_percentage = (self.round - self.opp_folded) / float(self.round)
+        if GP_percentage < 0.28:
+            tightness = False
 
-    return {
-      'tightness': tightness,
-      'aggressiveness': aggressiveness
-    }
+        aggressiveness = True
+        if self.opp_raise - self.opp_call <= 0:
+            aggressiveness = False
 
-  def call_or_check(self):
-    if self.unanswered_raise:
-      # any action including call will answer the raise thus flip it here
-      self.unanswered_raise = False
-      return 'call'
-    else:
-      # no opponent unanswered raise so a call would cost 0 -> effectively a check
-      return 'check'
+        return {
+            'tightness': tightness,
+            'aggressiveness': aggressiveness
+        }
 
-  # Utility functions to get observable environment var
-  def get_pot(self, round_state):
-    return round_state['pot']['main']['amount']      
+    def call_or_check(self):
+        if self.unanswered_raise:
+            # any action including call will answer the raise thus flip it here
+            self.unanswered_raise = False
+            return 'call'
+        else:
+            # no opponent unanswered raise so a call would cost 0 -> effectively a check
+            return 'check'
 
-  # Get our own stack. For opponent stack = 1000 - our stack - pot
-  def get_stack(self, round_state):
-    seats = round_state['seats']
-    if seats[0]['uuid'] == self.uuid:
-      return seats[0]['stack']
-    else:
-      return seats[1]['stack'] 
+    # Utility functions to get observable environment var
+    def get_pot(self, round_state):
+        return round_state['pot']['main']['amount']
+
+    # Get our own stack. For opponent stack = 1000 - our stack - pot
+    def get_stack(self, round_state):
+        seats = round_state['seats']
+        if seats[0]['uuid'] == self.uuid:
+            return seats[0]['stack']
+        else:
+            return seats[1]['stack']
 
 def setup_ai():
     return RandomPlayer()
